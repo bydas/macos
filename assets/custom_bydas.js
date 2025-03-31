89712f984aca1273816974ad67ba1741877d7e71
Bydas <info@bydas.com>
2025-03-31 09:01:07 +0100

    Feat: add macos

---
+++ assets/custom_bydas.js
@@ -0,0 +1,85 @@
+/* ----- Flickity Carousel - CATEGORIES ----- */
+$('#categories-carousel').flickity({
+  groupCells: false,
+  cellAlign: 'left',
+  contain: true,
+  freeScroll: false,
+  pageDots: false,
+  draggable: true
+});
+
+
+/* ----- Flickity Carousel - SLIDESHOW ----- */
+$('#highlights-slideshow').flickity({
+  autoPlay: true,
+  groupCells: false,
+  cellAlign: 'left',
+  contain: true,
+  pageDots: true,
+  draggable: false,
+  resize: true,
+  adaptiveHeight: false,
+  wrapAround: true
+});
+
+$(window).on('resize', function() {
+  $('#highlights-slideshow').flickity('resize');
+});
+
+
+/* ----- Flickity Carousel - BRANDS ----- */
+var $carousel = $('#main-carousel').flickity({
+  autoPlay: false,
+  wrapAround: true,
+  freeScroll: true,
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+  cellAlign: 'left',
+  contain: true
+});
+
+// start continuous scroll
+let flkty = $carousel.data('flickity');
+let isPaused = false; // pause control
+
+function flickityMarquee() {
+  if (!isPaused) {
+    flkty.x -= 1; // change the number to adjust the speed
+    flkty.positionSlider(); // aplies the movement
+  }
+  requestAnimationFrame(flickityMarquee);
+}
+
+// starts continuous animation
+flickityMarquee();
+
+// pause on hover
+$carousel.on('mouseenter', function() {
+  isPaused = false; // true / false to pause on hover
+});
+$carousel.on('mouseleave', function() {
+  isPaused = false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
